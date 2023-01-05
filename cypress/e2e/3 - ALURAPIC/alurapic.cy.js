592294386e018dc68fdada21d8c6ce7e02330b0f
Felipe <felipepg@uniplaclages.edu.br>
2023-01-05 16:34:57 -0300

    Criado novos testes

--- cypress/e2e/3 - ALURAPIC/alurapic.cy.js
+++ cypress/e2e/3 - ALURAPIC/alurapic.cy.js
@@ -18,4 +18,25 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
+
+    it('verifica mensagem de email inválido', () => {
+        //Act
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('felipe');
+
+        //Assert
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');       
+    })
+
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
+        //Act
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+
+        //Assert
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');       
+    })
 })
